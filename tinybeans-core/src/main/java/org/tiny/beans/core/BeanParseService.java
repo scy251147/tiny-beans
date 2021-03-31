14abec19df98f5af5e2f49d5438080e0e68765b3
shichaoyang <shichaoyang@jd.com>
2021-03-31 12:27:44 +0800

    split the classes biz

--- tinybeans-core/src/main/java/org/tiny/beans/core/BeanParseService.java
+++ tinybeans-core/src/main/java/org/tiny/beans/core/BeanParseService.java
@@ -1,9 +1,78 @@
 package org.tiny.beans.core;
 
+import org.tiny.beans.core.model.BeanDefinition;
+import org.tiny.beans.sdk.annotation.Bean;
+import org.tiny.beans.sdk.annotation.Scope;
+import org.tiny.beans.sdk.func.BeanPost;
+import org.tiny.beans.sdk.model.ScopeType;
+
+import java.lang.reflect.InvocationTargetException;
+
 /**
  * @author shichaoyang
  * @Description: bean解析annotation并处理
  * @date 2021-03-29 14:47
  */
 public class BeanParseService {
+
+    public BeanParseService(){
+
+    }
+
+    protected  void parse(){
+        //类解析并放到池子中
+        for (Class clazz : classes) {
+            if (clazz.isAnnotationPresent(Bean.class)) {
+
+                //如果实现了beanpost接口
+                if(BeanPost.class.isAssignableFrom(clazz)){
+                    try {
+                        BeanPost o = (BeanPost) clazz.getDeclaredConstructor().newInstance();
+                        beanPosts.add(o);
+                    } catch (InstantiationException e) {
+                        e.printStackTrace();
+                    } catch (IllegalAccessException e) {
+                        e.printStackTrace();
+                    } catch (InvocationTargetException e) {
+                        e.printStackTrace();
+                    } catch (NoSuchMethodException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                BeanDefinition beanDefinition = new BeanDefinition();
+                beanDefinition.setBeanClass(clazz);
+
+                Bean beanAnnotation = (Bean) clazz.getAnnotation(Bean.class);
+                String beanName = beanAnnotation.value();
+
+                //1. 解析原型模式/单例模式
+                //原型
+                if (clazz.isAnnotationPresent(Scope.class)) {
+                    Scope scopeAnnotation = (Scope) clazz.getAnnotation(Scope.class);
+                    beanDefinition.setScopeType(scopeAnnotation.value());
+                }
+                //单例
+                else {
+                    beanDefinition.setScopeType(ScopeType.Singleton);
+                }
+                beanDefinitionMap.put(beanName, beanDefinition);
+
+                //2. 解析懒加载模式
+                //TODO
+            }
+        }
+
+        //2. 实例化bean
+        for(String beanName: beanDefinitionMap.keySet()) {
+            BeanDefinition beanDefinition = beanDefinitionMap.get(beanName);
+            //创建单例bean
+            if (beanDefinition.getScopeType() == ScopeType.Singleton) {
+                if (!singletonObjectPool.containsKey(beanName)) {
+                    Object bean = createBean(beanName, beanDefinition);
+                    singletonObjectPool.put(beanName, bean);
+                }
+            }
+        }
+    }
 }
--- tinybeans-core/src/main/java/org/tiny/beans/core/BeanServiceFactory.java
+++ tinybeans-core/src/main/java/org/tiny/beans/core/BeanServiceFactory.java
@@ -7,34 +7,79 @@ package org.tiny.beans.core;
  */
 public class BeanServiceFactory {
 
-    public BeanServiceFactory(){
-
+    /**
+     * 带参构造
+     * @param configClass
+     */
+    public BeanServiceFactory(Class<?> configClass){
+        this.configClass = configClass;
     }
 
+    //TODO 串了这么多类，建议搞个上下文对象
+
+    //扫描bean服务
     private static volatile BeanScanService beanScanService;
 
+    //创建bean服务
     private static volatile BeanCreateService beanCreateService;
 
+    //解析bean服务
     private static volatile BeanParseService beanParseService;
 
+    //扫描bean服务实例锁
     private static Object beanScanLock = new Object();
 
+    //创建bean服务实例锁
     private static Object beanCreateLock = new Object();
 
+    //解析bean服务实例锁
     private static Object beanParseLock = new Object();
 
-    protected BeanScanService getBeanScanServiceInstance(){
-        if(beanScanService == null){
-            synchronized (beanScanLock){
+    //扫描路径配置类
+    private Class<?> configClass;
+
+    /**
+     * 创建beanscan服务实例
+     * @return
+     */
+    protected BeanScanService getBeanScanServiceInstance() {
+        if (beanScanService == null) {
+            synchronized (beanScanLock) {
                 if (beanScanService == null) {
-                    return new BeanScanService();
+                    beanScanService = new BeanScanService(configClass);
                 }
             }
         }
+        return beanScanService;
     }
 
+    /**
+     * 创建beancreate服务实例
+     * @return
+     */
+    protected BeanCreateService getBeanCreateServiceIntance() {
+        if (beanCreateService == null) {
+            synchronized (beanCreateLock) {
+                if (beanCreateService == null) {
+                    beanCreateService = BeanCreateService();
+                }
+            }
+        }
+        return beanCreateService;
+    }
 
-    
-
-
+    /**
+     * 创建beanparse服务实例
+     * @return
+     */
+    protected  BeanParseService getBeanParseServiceInstance() {
+        if (beanParseService == null) {
+            synchronized (beanParseLock) {
+                if (beanParseService == null) {
+                    beanParseService = BeanParseService();
+                }
+            }
+        }
+        return beanParseService;
+    }
 }
